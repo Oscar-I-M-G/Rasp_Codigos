ffe1af426ad276088fa7fad26011dcf1ae54b177
Oscar Ivan <oscar.moreno4275@alumnos.udg.mx>
2024-11-13 14:28:05 -0600

    - se Corrigieron unos errores

--- sendTempData.py
+++ sendTempData.py
@@ -4,20 +4,36 @@ import threading
 import adafruit_dht
 import digitalio
 import mysql.connector as mariadb
+import os
+from datetime import datetime
 
 
-# Setting up the DHT sensor
-dht_device = adafruit_dht.DHT11(board.D22)
+# Poniendo el sensor DHT11 en el pin 22
+try:
+    dht_device = adafruit_dht.DHT11(board.D22)
+    print("Sensor DHT11 puesto correctamente")
+except Exception as e:
+    print(f"Error poniendo el sensor DHT11: {e}")
+    exit()
 
-# Setting up the button
-button = digitalio.DigitalInOut(board.D27)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
+# Configurando el boton
+try:
+    button = digitalio.DigitalInOut(board.D27)
+    button.direction = digitalio.Direction.INPUT
+    button.pull = digitalio.Pull.UP
+    print("Buton puesto correctamente")
+except Exception as e:
+    print(f"Error poniendo el boton: {e}")
+    exit()
 
-# Setting up the LED
-led = digitalio.DigitalInOut(board.D17)
-led.direction = digitalio.Direction.OUTPUT
-
+# configurando el LED
+try:
+    led = digitalio.DigitalInOut(board.D17)
+    led.direction = digitalio.Direction.OUTPUT
+    print("LED puesto correctamente")
+except Exception as e:
+    print(f"Error poniendo el LED: {e}")
+    exit()
 # List to store the data
 temperature_data = []
 
@@ -26,7 +42,7 @@ db_config = {
     'user': 'root',
     'password': 'root',
     'host': 'localhost',
-    'database': 'temperaturas'
+    'database': 'temperaturasdb'
 }
 
 
@@ -35,6 +51,12 @@ stop_sensor = threading.Event()
 # Event to stop the button
 stop_button = threading.Event()
 
+#Thread safety
+temperature_data_lock = threading.Lock()
+
+def clear_screen():
+    os.system('clear')
+
 def read_dht_sensor():
     while not stop_sensor.is_set():
         try:
@@ -42,21 +64,26 @@ def read_dht_sensor():
             humidity = dht_device.humidity
 
             if humidity is not None and temperature is not None:
-                #print(f"Temp={temperature:.1f}C  Humidity={humidity:.1f}%")
-                temperature_data.append((temperature, humidity))
+                
+                ahora = datetime.now()
+                fecha = now.date()
+                hora = now.time()
+                temperature_data.append((fecha, hora, humidity, temperature))
             else:
-                print("Failed to retrieve data from humidity sensor")
+                print("Error leyendo el sensor")
         except RuntimeError as error:
             print(error.args[0])
         time.sleep(2)
 
 def send_data_to_db(data):
+    conn = None
+    cursor = None
     try:
         conn = mariadb.connect(**db_config)
         cursor = conn.cursor()
-        cursor.executemany("INSERT INTO temperature_readings (temperature, humidity) VALUES (%s, %s)", data)
+        cursor.executemany("INSERT INTO temperature_readings (FECHA, HORA, HUMEDAD, TEMPERATURA) VALUES (%s, %s,%s,%s)", data)
         conn.commit()
-        print("Data sent successfully")
+        print("Datos mandados correctamente")
     except mariadb.Error as err:
         print(f"Error: {err}")
     finally:
@@ -70,8 +97,10 @@ def check_button():
     while not stop_button.is_set():
         if not button.value:  # Button is pressed (assuming active low)
             led.value = True  # Toggle the LED
-            send_data_to_db(temperature_data)
-            temperature_data.clear()  # Clear the list after sending
+            with temperature_data_lock:
+                if len(temperature_data) > 0:
+                    send_data_to_db(temperature_data)
+                    temperature_data.clear()  # Clear the list after sending the data
             time.sleep(0.2)  # Debounce delay
 
 sensor_thread = threading.Thread(target=read_dht_sensor)
@@ -84,12 +113,15 @@ button_thread.start()
 
 try:
     while True:
+        with temperature_data_lock:
+            print(f"Datos listos para guardar {len(temperature_data)} ")
+        clear_screen()
         time.sleep(1)  # Reduce CPU usage
 except KeyboardInterrupt:
-    print("Exiting program")
+    print("Saliendo del programa")
     stop_sensor.set()
     stop_button.set()
 finally:
     sensor_thread.join()
     button_thread.join()
-    print("Program has ended")
+    print("Programa a terminado")
